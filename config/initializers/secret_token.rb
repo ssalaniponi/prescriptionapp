e2b7d2f61193c70a14985580154b1277ef1aca14
ssalaniponi <steven_salaniponi@yahoo.co.uk>
2016-01-10 18:26:24 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prescriptionapp::Application.config.secret_key_base = 'b4caecaf41ec4db7906fe9817330d9d86a06caa65f6a5c14a2e2306591ad8f488e736c8ed333c62bbbae05bf2cdfe2ad2aec5da0a60f363a47b956570c868d55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Prescriptionapp::Application.config.session_store :cookie_store, key: '_prescriptionapp_session'
